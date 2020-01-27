f1efedf62ff3a873fb871a8933640530a40ecbdf
Seif <seif@seif.rock>
2020-01-27 12:08:38 +0200

    added vlidation

--- routes/api/words.js
+++ routes/api/words.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const {getLD} = require('../../util/getLD');
+const { getLD } = require('../../util/getLD');
 const NonLexWord = require('../../models/Word');
+const { check, validationResult } = require('express-validator');
 
 // @route	GET api/words
 // @desc	Get Lexical Density of text
 // @access	Public
-router.get('/', async (req, res)	=>	{
+router.get('/', [
+	check('text')
+		.exists()
+		.isLength({ max: 1000 })
+		.withMessage('Text is too long; must be less than 1000 characters')
+		.custom(text => {
+			if (wordCounter(text) > 100) {
+				return Promise.reject('Text is too long; must be less than 100 words')
+			}
+		})
+], async (req, res) => {
 
-	const sentenceWithPunctuations = req.body.sentence.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	const sentences = sentenceWithPunctuations.replace(/\s{2,}/g," ").toLowerCase().split(".");
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+	}
+
+	let sentences = sentencesWithoutPunctuations(req.body.text)
+	sentences = sentences.replace(/\s{2,}/g, " ").toLowerCase().split(".");
 	let nonLexicalWords = await NonLexWord.find({});
 	var lexicalDensities = [];
 
-	// sentences.forEach(sentence => getLD(sentence, (ld) => lexicalDensities.push(ld)));
 	for await (sentence of sentences) {
-		 const ld = getLD(sentence,nonLexicalWords);
-		 lexicalDensities.push(ld);
+		const ld = getLD(sentence, nonLexicalWords);
+		lexicalDensities.push(ld);
 	}
 	console.log(lexicalDensities);
+
 	// Calculate overall LD
 	const numberOfSentences = sentences.length;
 	let sumOfLDs = 0;
-	
+
 	lexicalDensities.forEach(ld => {
 		sumOfLDs += ld;
 	});
@@ -30,7 +46,7 @@ router.get('/', async (req, res)	=>	{
 	const overallLD = parseFloat((sumOfLDs / numberOfSentences).toFixed(2));
 	const verbose = req.query.mode === "verbose";
 
-	if(verbose) {
+	if (verbose) {
 		res.json({
 			data: {
 				sentence_ld: lexicalDensities,
@@ -48,5 +64,13 @@ router.get('/', async (req, res)	=>	{
 	}
 
 });
+
+function sentencesWithoutPunctuations(sentences) {
+	return sentences.replace(/[,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function wordCounter(sentences) {
+	return sentences.split(' ').length;
+}
 
 module.exports = router;
